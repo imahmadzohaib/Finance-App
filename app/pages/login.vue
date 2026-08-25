39b891e2421ff104558500d5881fa5ba8e57c73b
Ahmad Zohaib <imahmadzohaib@gmail.com>
2026-08-25 17:42:57 +0500

    miss spelled

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -43,7 +43,7 @@ const handleLogin = async ()=>{
         const {error} = await supabase.auth.signInWithOtp({
             email:email.value,
             options:{
-                emailRedirectTo: `$${redirectUrl}/confirm`
+                emailRedirectTo: `${redirectUrl}/confirm`
             }
         })
         if(error){
